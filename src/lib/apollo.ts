6e6fc0ac2e52e9c25004e81526478b78bee0c976
Somnath Mishra <somnath.mishra@deskera.org>
2024-12-01 10:13:32 +0530

    final changes

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,10 +1,22 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
 
 const httpLink = createHttpLink({
-  uri: 'https://dada-dada.hypermode.app/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_API_URL,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = import.meta.env.VITE_GRAPHQL_API_TOKEN;
+
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    }
+  };
 });
 
 export const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 }); 
