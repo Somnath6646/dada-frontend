3f39d1985bdf10e355e1e2ee9ee721311dbf6a57
Somnath Mishra <somnath.mishra@deskera.org>
2024-12-02 01:09:30 +0530

    added vercel analytics

--- src/main.tsx
+++ src/main.tsx
@@ -4,11 +4,13 @@ import { ApolloProvider } from "@apollo/client";
 import { client } from "./lib/apollo";
 import "./index.css";
 import App from "./App";
+import { Analytics } from "@vercel/analytics/react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ApolloProvider client={client}>
       <App />
+      <Analytics />
     </ApolloProvider>
   </React.StrictMode>
 );
